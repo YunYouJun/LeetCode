641f4b194a17c7654cc65bcf54a7f58daab0be9c
云游君 <me@yunyoujun.cn>
2019-03-03 14:10:47 +0800

    fix paste answer

--- problems/single-number/solution-2.py
+++ problems/single-number/solution-2.py
@@ -1,11 +1,11 @@
 class Solution:
     def singleNumber(self, nums: [int]) -> int:
         dic = {}
-        for num in enumerate(nums):
+        for num in nums:
             if num in dic:
                 dic[num] += 1
             else:
                 dic[num] = 1
-        for num in enumerate(nums):
+        for num in nums:
             if dic[num] == 1:
                 return num
