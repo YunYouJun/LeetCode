d5e4a4a3f73dfea3d4064d947f654b9ea891e800
云游君 <me@yunyoujun.cn>
2019-04-21 16:55:05 +0800

    join 2019-spring final

---
+++ contest/college/2019-spring/lexicographically-smallest-equivalent-string/solution.js
@@ -0,0 +1,42 @@
+/**
+ * @param {string} A
+ * @param {string} B
+ * @param {string} S
+ * @return {string}
+ */
+var smallestEquivalentString = function(A, B, S) {
+  let dic = {}
+  for (let i = 0; i < 26; i++) {
+    dic[String.fromCharCode(97 + i)] = String.fromCharCode(97 + i)
+  }
+
+  function get(x) {
+    if (dic[x] === x) return x
+    return dic[x] = get(dic[x])
+  }
+
+  function merge(x, y) {
+    x = get(x)
+    y = get(y)
+    if (x < y) {
+      let temp = y
+      y = x
+      x = temp
+    }
+    dic[x] = y
+  }
+
+  for (let i = 0; i < A.length; i++) {
+    merge(A[i], B[i])
+  }
+
+  let res = ''
+  for (let i = 0; i < S.length; i++) {
+    res += get(S[i])
+  }
+  return res
+}
+
+// smallestEquivalentString("parker", "morris", "parser")
+// smallestEquivalentString("hello", "world", "hold")
+smallestEquivalentString('leetcode', 'programs', 'sourcecode')
---
+++ contest/college/2019-spring/missing-element-in-sorted-array/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var missingElement = function(nums, k) {
+  let n = nums[0]
+  for (let i = 1; i < nums.length; i++) {
+    while (n < nums[i] && k) {
+      n++
+      k--
+      if (n === nums[i]) k++
+    }
+  }
+  while (k) {
+    n++
+    k--
+  }
+  return n
+}
