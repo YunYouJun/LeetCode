a89160c6220e9553d78ef846c1f93463e8dea0ef
YunYouJun <me@yunyoujun.cn>
2019-05-14 11:40:32 +0800

    solve add-to-array-form-of-integer

---
+++ problems/add-to-array-form-of-integer/solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} A
+ * @param {number} K
+ * @return {number[]}
+ */
+var addToArrayForm = function(A, K) {
+  A[A.length-1] += K
+  let carry = 0
+  for (let i = A.length - 1; i >= 0; i--) {
+    carry = parseInt(A[i] / 10)
+    A[i] = A[i] % 10
+    if (i) A[i-1] += carry
+  }
+  if (carry) {
+    A = carry.toString().split('').concat(A)
+  }
+  return A
+}
