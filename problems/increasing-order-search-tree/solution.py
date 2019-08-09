c1d3f90af4d6e3e4705898c0f95bcb331f85ff87
YunYouJun <me@yunyoujun.cn>
2019-08-09 22:45:48 +0800

    solve increasing-order-search-tree

---
+++ problems/increasing-order-search-tree/solution.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def increasingBST(self, root: TreeNode) -> TreeNode:
+        def inorder(node):
+            if node:
+                inorder(node.left)
+                node.left = None
+                self.cur.right = node
+                self.cur = node
+                inorder(node.right)
+
+        ans = self.cur = TreeNode(None)
+        inorder(root)
+        return ans.right
