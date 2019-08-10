8ab1441a36ca7fa56a87d8d518c4b0e8df057389
YunYouJun <me@yunyoujun.cn>
2019-08-10 23:35:36 +0800

    solve binary-tree-paths

---
+++ problems/binary-tree-paths/solution-1.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def binaryTreePaths(self, root: TreeNode) -> [str]:
+        def construct_paths(root, path):
+            if root:
+                path += str(root.val)
+                if not root.left and not root.right:
+                    paths.append(path)
+                else:
+                    path += '->'
+                    construct_paths(root.left, path)
+                    construct_paths(root.right, path)
+        
+        paths = []
+        construct_paths(root, '')
+        return paths
