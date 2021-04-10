4df267149e146b0cb8447d2bf32be54b5b90983d
YunYouJun <me@yunyoujun.cn>
2021-04-10 14:39:31 +0800

    ✅ solve longest-common-subsequence

---
+++ problems/longest-common-subsequence/solution.ts
@@ -0,0 +1,26 @@
+function longestCommonSubsequence(text1: string, text2: string): number {
+  const m = text1.length;
+  const n = text2.length;
+  const dp = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
+
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (text1[i - 1] === text2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+
+  return dp[m][n];
+}
+
+import { testFunction } from "../../utils";
+const testcases = [
+  ["abcde", "ace"],
+  ["abc", "abc"],
+  ["abc", "def"],
+];
+const expectedReults = [3, 3, 0];
+testFunction(longestCommonSubsequence, testcases, expectedReults);
