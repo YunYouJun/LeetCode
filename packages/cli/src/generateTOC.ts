a0e32680fb0f4037734e72b1c3e77f12a45d162d
YunYouJun <me@yunyoujun.cn>
2022-07-07 02:11:29 +0800

    ci: fix generate by ts

--- packages/cli/src/generateTOC.ts
+++ packages/cli/src/generateTOC.ts
@@ -1,36 +1,31 @@
 import fs from 'node:fs'
-import {
-  getAllProblems,
-  generateProblemMd,
-  readLanguageByExt,
-} from "./toc"
-
-const distFolder = "./dist";
+import { getAllProblems, generateProblemMd, readLanguageByExt } from './toc'
+import { logger } from './logger'
+const distFolder = './dist'
 
 /**
  * 生成目录
  */
 export function generateToc() {
   // Get yaml info
-  let problems = getAllProblems();
+  let problems = getAllProblems()
   let toc_md = `# Problems (${problems.length})
 
 |#|Title|Solution|Difficulty|Language|
 |-|-----|--------|----------|--------|
-`;
+`
 
   for (let i = 0; i < problems.length; i++) {
-    let problem = problems[i];
-    problem.language = readLanguageByExt(problem);
-    let problem_md = generateProblemMd(problem);
-    toc_md += problem_md;
+    let problem = problems[i]
+    problem.language = readLanguageByExt(problem)
+    let problem_md = generateProblemMd(problem)
+    toc_md += problem_md
   }
 
   if (!fs.existsSync(distFolder)) {
-    fs.mkdirSync(distFolder);
+    fs.mkdirSync(distFolder)
   }
 
-  fs.writeFileSync(`${distFolder}/README.md`, toc_md);
-  const logger = require("./logger");
-  logger.info("生成 LeetCode 目录~");
+  fs.writeFileSync(`${distFolder}/README.md`, toc_md)
+  logger.info('生成 LeetCode 目录~')
 }
